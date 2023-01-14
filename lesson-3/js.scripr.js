e44490a6e27b3435a263772feedde655966553da
geekbrains <info@geekbrains.ru>
2023-01-14 12:29:40 +0300

    Second commit

---
+++ lesson-3/js.scripr.js
@@ -0,0 +1,35 @@
+"use strict"
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/Windows/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+        }
+
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
+}    
+
+
